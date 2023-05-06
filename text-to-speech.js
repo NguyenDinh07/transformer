f9ae297fe512d15b8413105d29c467c81693fc8f
Harupip <chudangnghia2002@gmail.com>
2023-05-06 22:45:13 +0700

    fix voice and update chatGPT

--- text-to-speech.js
+++ text-to-speech.js
@@ -14,10 +14,18 @@
 
 // speechSynthesis.speak(msg);
 
+const xSpeech = {};
+
+// Capture a Web Speech Synthesis Instance
+xSpeech.synth = window.speechSynthesis || null;
+
+// Voices available
+xSpeech.voices = [];
 document.getElementById('speaker').addEventListener('click', () => {
-    var msg = document.getElementById('text-to-speech__input').value
+    var msg = document.getElementById('text-to-speech__input').value.trim();
+    console.log(xSpeech.voices = xSpeech.synth.getVoices())
     const utterance = new SpeechSynthesisUtterance(msg)
-    utterance.rate = 1
+    utterance.voice = xSpeech.voices[3];
     speechSynthesis.speak(utterance)
 })
 
@@ -29,8 +37,50 @@ document.addEventListener('keyup', (event) => {
     if(event.ctrlKey && event.key === ' ') {
         var msg = document.getElementById('text-to-speech__input').value
         const utterance = new SpeechSynthesisUtterance(msg)
-        utterance.rate = 1
+        utterance.voice = xSpeech.voices[3];
         speechSynthesis.speak(utterance)
     }
 });
 
+
+const API_KEY = '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';
+
+document.getElementById('speaker2').addEventListener('click', async (e) => {
+    e.preventDefault();
+    console.log(xSpeech.voices = xSpeech.synth.getVoices())
+    var msg = document.getElementById('text-to-speech__input2').value.trim();
+    if (msg) {
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [{ role: 'user', content: msg }],
+                    temperature: 1.0,
+                    top_p: 0.7,
+                    n: 1,
+                    stream: false,
+                    presence_penalty: 0,
+                    frequency_penalty: 0,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                msg = data.choices[0].message.content;
+            } else {
+                msg = 'Error: Unable to process your request.';
+            }
+        } catch (error) {
+            console.error(error);
+            msg = 'Error: Unable to process your request.';
+        }
+    }
+    const utterance = new SpeechSynthesisUtterance(msg)
+    utterance.voice = xSpeech.voices[3];
+    speechSynthesis.speak(utterance)
+})
